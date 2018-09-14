96502ec8e6d6387aebabe4b7c5130dfd53dc3543
JellyLu <634725546@qq.com>
2018-09-14 13:52:15 +0800

    update path

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker-demo/src/sw.js',
+    navigator.serviceWorker.register('/service-worker-demo/sw.js',
         {scope: '/service-worker-demo/'}
     ).then(function (reg) {
 
